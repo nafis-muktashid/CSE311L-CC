07ac4165466e141b0348cd64f91e2b8e431a1e7a
Md. Nafis Muktashid Arpon <nafismuktashid@gmail.com>
2025-04-02 13:17:44 +0600

    Add files via upload

---
+++ database/connectcore.sql
@@ -0,0 +1,496 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 19, 2025 at 05:50 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `connectcore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activitylog`
+--
+
+CREATE TABLE `activitylog` (
+  `logId` int(11) NOT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `action` text NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `audit_logs`
+--
+
+CREATE TABLE `audit_logs` (
+  `log_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `action_type` varchar(255) NOT NULL,
+  `action_details` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companies`
+--
+
+CREATE TABLE `companies` (
+  `companyId` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `industry_type` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `registered_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company_subscriptions`
+--
+
+CREATE TABLE `company_subscriptions` (
+  `subscription_id` int(11) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  `plan_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contracts`
+--
+
+CREATE TABLE `contracts` (
+  `contractId` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `status` enum('active','completed','terminated') DEFAULT 'active',
+  `hiringCompanyId` int(11) DEFAULT NULL,
+  `applyingCompanyId` int(11) DEFAULT NULL,
+  `jobId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `disputes`
+--
+
+CREATE TABLE `disputes` (
+  `dispute_id` int(11) NOT NULL,
+  `contract_id` int(11) NOT NULL,
+  `raised_by_company_id` int(11) NOT NULL,
+  `against_company_id` int(11) NOT NULL,
+  `reason` text NOT NULL,
+  `status` enum('Open','In Progress','Resolved','Closed') DEFAULT 'Open',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employeeassignments`
+--
+
+CREATE TABLE `employeeassignments` (
+  `recordId` int(11) NOT NULL,
+  `contractId` int(11) DEFAULT NULL,
+  `companyId` int(11) DEFAULT NULL,
+  `employeeId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `employeeId` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `position` varchar(100) DEFAULT NULL,
+  `rate` decimal(10,2) DEFAULT NULL,
+  `availability_status` enum('available','unavailable') DEFAULT NULL,
+  `companyId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employeeskills`
+--
+
+CREATE TABLE `employeeskills` (
+  `employeeId` int(11) NOT NULL,
+  `skillId` int(11) NOT NULL,
+  `experience_level` enum('beginner','intermediate','expert') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobapplications`
+--
+
+CREATE TABLE `jobapplications` (
+  `applicationId` int(11) NOT NULL,
+  `applyingCompanyId` int(11) DEFAULT NULL,
+  `jobId` int(11) DEFAULT NULL,
+  `apply_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` enum('pending','accepted','rejected') DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobpostings`
+--
+
+CREATE TABLE `jobpostings` (
+  `jobId` int(11) NOT NULL,
+  `job_title` varchar(255) NOT NULL,
+  `required_skill` int(11) DEFAULT NULL,
+  `job_details` text DEFAULT NULL,
+  `rate` decimal(10,2) DEFAULT NULL,
+  `status` enum('open','closed') DEFAULT 'open',
+  `posted_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `companyId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `notificationId` int(11) NOT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `message` text NOT NULL,
+  `read_status` tinyint(1) DEFAULT 0,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `paymentId` int(11) NOT NULL,
+  `contractId` int(11) DEFAULT NULL,
+  `total_amount` decimal(10,2) NOT NULL,
+  `applyingCompanyShare` decimal(10,2) NOT NULL,
+  `skillConnetShare` decimal(10,2) NOT NULL,
+  `employee_salary` decimal(10,2) DEFAULT NULL,
+  `payment_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` enum('pending','completed','failed') DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills`
+--
+
+CREATE TABLE `skills` (
+  `skillId` int(11) NOT NULL,
+  `skill_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscription_plans`
+--
+
+CREATE TABLE `subscription_plans` (
+  `plan_id` int(11) NOT NULL,
+  `plan_name` varchar(100) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `features` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userId` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone_num` varchar(20) DEFAULT NULL,
+  `user_type` enum('admin','company') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activitylog`
+--
+ALTER TABLE `activitylog`
+  ADD PRIMARY KEY (`logId`),
+  ADD KEY `fk_user` (`userId`);
+
+--
+-- Indexes for table `audit_logs`
+--
+ALTER TABLE `audit_logs`
+  ADD PRIMARY KEY (`log_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `companies`
+--
+ALTER TABLE `companies`
+  ADD PRIMARY KEY (`companyId`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `company_subscriptions`
+--
+ALTER TABLE `company_subscriptions`
+  ADD PRIMARY KEY (`subscription_id`),
+  ADD KEY `company_id` (`company_id`),
+  ADD KEY `plan_id` (`plan_id`);
+
+--
+-- Indexes for table `contracts`
+--
+ALTER TABLE `contracts`
+  ADD PRIMARY KEY (`contractId`),
+  ADD KEY `hiringCompanyId` (`hiringCompanyId`),
+  ADD KEY `applyingCompanyId` (`applyingCompanyId`),
+  ADD KEY `jobId` (`jobId`);
+
+--
+-- Indexes for table `disputes`
+--
+ALTER TABLE `disputes`
+  ADD PRIMARY KEY (`dispute_id`),
+  ADD KEY `contract_id` (`contract_id`),
+  ADD KEY `raised_by_company_id` (`raised_by_company_id`),
+  ADD KEY `against_company_id` (`against_company_id`);
+
+--
+-- Indexes for table `employeeassignments`
+--
+ALTER TABLE `employeeassignments`
+  ADD PRIMARY KEY (`recordId`),
+  ADD KEY `contractId` (`contractId`),
+  ADD KEY `companyId` (`companyId`),
+  ADD KEY `employeeId` (`employeeId`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employeeId`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `companyId` (`companyId`);
+
+--
+-- Indexes for table `employeeskills`
+--
+ALTER TABLE `employeeskills`
+  ADD PRIMARY KEY (`employeeId`,`skillId`),
+  ADD KEY `skillId` (`skillId`);
+
+--
+-- Indexes for table `jobapplications`
+--
+ALTER TABLE `jobapplications`
+  ADD PRIMARY KEY (`applicationId`),
+  ADD KEY `applyingCompanyId` (`applyingCompanyId`),
+  ADD KEY `jobId` (`jobId`);
+
+--
+-- Indexes for table `jobpostings`
+--
+ALTER TABLE `jobpostings`
+  ADD PRIMARY KEY (`jobId`),
+  ADD KEY `companyId` (`companyId`),
+  ADD KEY `required_skill` (`required_skill`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`notificationId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`paymentId`),
+  ADD KEY `contractId` (`contractId`);
+
+--
+-- Indexes for table `skills`
+--
+ALTER TABLE `skills`
+  ADD PRIMARY KEY (`skillId`),
+  ADD UNIQUE KEY `skill_name` (`skill_name`);
+
+--
+-- Indexes for table `subscription_plans`
+--
+ALTER TABLE `subscription_plans`
+  ADD PRIMARY KEY (`plan_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activitylog`
+--
+ALTER TABLE `activitylog`
+  MODIFY `logId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `audit_logs`
+--
+ALTER TABLE `audit_logs`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `companies`
+--
+ALTER TABLE `companies`
+  MODIFY `companyId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `company_subscriptions`
+--
+ALTER TABLE `company_subscriptions`
+  MODIFY `subscription_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `contracts`
+--
+ALTER TABLE `contracts`
+  MODIFY `contractId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `disputes`
+--
+ALTER TABLE `disputes`
+  MODIFY `dispute_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employeeassignments`
+--
+ALTER TABLE `employeeassignments`
+  MODIFY `recordId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `employeeId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jobapplications`
+--
+ALTER TABLE `jobapplications`
+  MODIFY `applicationId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jobpostings`
+--
+ALTER TABLE `jobpostings`
+  MODIFY `jobId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `notificationId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `paymentId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `skills`
+--
+ALTER TABLE `skills`
+  MODIFY `skillId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subscription_plans`
+--
+ALTER TABLE `subscription_plans`
+  MODIFY `plan_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `activitylog`
+--
+ALTER TABLE `activitylog`
+  ADD CONSTRAINT `fk_activitylog_user` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `company_subscriptions`
+--
+ALTER TABLE `company_subscriptions`
+  ADD CONSTRAINT `company_subscriptions_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `companies` (`companyId`),
+  ADD CONSTRAINT `company_subscriptions_ibfk_2` FOREIGN KEY (`plan_id`) REFERENCES `subscription_plans` (`plan_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
